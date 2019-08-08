d9dbbc85082786fcd3029a9af416e53027740521
Wiley <throughskybrim@gmail.com>
2019-08-08 22:06:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ else
     echo "You are in toutiao network, no need to use ss now"
 fi
 
+brew tap caskroom/cask
+
 if [[ ! -e /Applications/iTerm.app ]]; then
     brew cask install iterm2
     defaults delete com.googlecode.iterm2
