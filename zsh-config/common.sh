b4da84419cc269540630f9f1d23a2b88e8f1d6ff
Wiley <throughskybrim@gmail.com>
2019-08-08 12:56:35 +0800

    zsh - common

--- zsh-config/common.sh
+++ zsh-config/common.sh
@@ -5,13 +5,13 @@ source $BS_ZSH_BASE/zsh-config/find.sh
 source $BS_ZSH_BASE/zsh-config/git.sh
 source $BS_ZSH_BASE/zsh-config/grep.sh
 source $BS_ZSH_BASE/zsh-config/misc.sh
-source $BS_ZSH_BASE/zsh-config/tools.sh
+# source $BS_ZSH_BASE/zsh-config/tools.sh
 source $BS_ZSH_BASE/zsh-config/alias.sh
 source $BS_ZSH_BASE/zsh-config/fzf.sh
-source $BS_ZSH_BASE/software/qiniu/qiniu.sh
+# source $BS_ZSH_BASE/software/qiniu/qiniu.sh
 for f in $BS_ZSH_BASE/zsh-config/functions/*.sh; do source $f; done
 
 # Script only for mac
 source $BS_ZSH_BASE/zsh-config/platform.mac.sh
-source $BS_ZSH_BASE/zsh-config/personalized.sh
+# source $BS_ZSH_BASE/zsh-config/personalized.sh
 source $BS_ZSH_BASE/zsh-config/colors.sh
