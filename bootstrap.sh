72f4431c6a293051c2e92b494eddcbf3cd3cb5c8
Wiley <throughskybrim@gmail.com>
2019-08-09 08:52:49 +0800

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ fi
 cd "$(brew --repo)"
 git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
 
-cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-core
 git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
 
 cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
