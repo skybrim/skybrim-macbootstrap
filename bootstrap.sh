fc9668ad21e352017a1e78c1d49960ca2933bd63
Wiley <throughskybrim@gmail.com>
2019-08-09 09:05:43 +0800

    .#

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,10 @@ fi
 cd "$(brew --repo)"
 git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
 
-cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-core
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
 git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
 
-cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-cask"
 git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
     
 echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.bash_profile
