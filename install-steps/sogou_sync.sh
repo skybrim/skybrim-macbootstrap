a269f40a3b7c9baffd64c101590bb967d7668226
Wiley <throughskybrim@gmail.com>
2019-08-09 14:58:45 +0800

    sogou

--- install-steps/sogou_sync.sh
+++ install-steps/sogou_sync.sh
@@ -1,7 +1,7 @@
 # Sync Sougou Config
-cp config/SogouPreferences.plist config/UserPreferences.plist
-mv config/UserPreferences.plist "$HOME/Library/Application Support/Sogou/InputMethod/SogouPY/"
+cp ~/.macbootstrap/config/SogouPreferences.plist ~/.macbootstrap/config/UserPreferences.plist
+mv ~/.macbootstrap/config/UserPreferences.plist "$HOME/Library/Application Support/Sogou/InputMethod/SogouPY/"
 
-cp config/sogouUserSet.plist config/autoEngUserSet.plist
-mv config/autoEngUserSet.plist "$HOME/Library/Application Support/Sogou/InputMethod/SogouPY/"
+cp ~/.macbootstrap/config/sogouUserSet.plist ~/.macbootstrap/config/autoEngUserSet.plist
+mv ~/.macbootstrap/config/autoEngUserSet.plist "$HOME/Library/Application Support/Sogou/InputMethod/SogouPY/"
 
