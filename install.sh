60875b39e6b3315a35d092a95825ec08a606a227
Wiley <throughskybrim@gmail.com>
2019-08-15 12:47:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,7 +126,6 @@ fi
 backup_file ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 ln -s ~/.macbootstrap/zsh-config/.zshrc ~/.zshrc
 chsh -s /bin/zsh
 
