1e9fb84274d20501a7f0cbcbebffab373e08deeb
skybrim <throughskybrim@gmail.com>
2019-10-10 09:12:31 +0800

    .#

--- clean.sh
+++ clean.sh
@@ -6,14 +6,14 @@ rm -f /usr/local/bin/gvim
 # brew cask uninstall iterm2
 # brew cask uninstall sourcetree
 # brew cask uninstall google-chrome
-# brew cask uninstall coderunner
+brew cask uninstall coderunner
 # brew cask uninstall the-unarchiver
 # brew cask uninstall visual-studio-code
 # brew uninstall wget
 # brew uninstall gnu-sed
 # brew uninstall autojump
 # brew uninstall cmake
-# brew uninstall nvm
+brew uninstall nvm
 # brew uninstall ctags
 # brew uninstall gawk
 # brew uninstall coreutils
