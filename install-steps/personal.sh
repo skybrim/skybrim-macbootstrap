46f0dfdb1b09b5d0bfbc62e6cd5bb83017c5b102
skybrim <throughskybrim@gmail.com>
2019-10-10 10:00:03 +0800

    gpg

--- install-steps/personal.sh
+++ install-steps/personal.sh
@@ -14,4 +14,9 @@ if [[ "$username" == $(whoami) ]]; then
     ln -s ~/.macbootstrap/zsh-config/ssh_config ~/.ssh/config
 
     # 配置 GPG
+    # GPG_KEY="$HOME/.macbootstrap/git-config/bestswifter.asc"
+    # gpg --import "$GPG_KEY"
+    # shred --remove "$GPG_KEY"
+    # git config --global user.signingkey 368B0D29D38D4B4EEE5BF51EB2468CF4358BF1CF
+    # git config --global commit.gpgsign true
 fi
