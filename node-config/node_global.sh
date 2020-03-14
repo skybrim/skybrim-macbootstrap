1aa107ed3702c2352e066ce05b689e8bff9d14c3
skybrim <throughskybrim@gmail.com>
2020-03-14 18:32:30 +0800

    npm

--- node-config/node_global.sh
+++ node-config/node_global.sh
@@ -5,7 +5,7 @@ npm config -g set registry https://registry.npm.taobao.org
 # npm install -g es-checker
 # npm install -g sequelize-auto
 # npm install -g bash-language-server
-npm install -g hexo-cli
-# npm install -g eslint-cli
 # npm install -g eslint-plugin-html
-# npm install -g js-beautify
+npm install -g js-beautify
+npm install -g hexo-cli
+npm install -g eslint
