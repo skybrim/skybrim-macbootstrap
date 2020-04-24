04b11ce45745b606ea1de32f515c410433bfa5c6
skybrim <throughskybrim@gmail.com>
2020-04-24 09:39:15 +0800

    main

--- main.sh
+++ main.sh
@@ -13,28 +13,42 @@ export ALL_PROXY=socks5://127.0.0.1:10009
 sudo apachectl start
 sudo ln -s ~/.macbootstrap/net-config/ss.pac /Library/WebServer/Documents/ss.pac
 else
-    echo "no need to use ss now"
+    echo ">>>>>>>>>> no need to use ss now <<<<<<<<<<"
 fi
 
 # zsh config
+echo ">>>>>>>>>> zsh_install run <<<<<<<<<<"
 sh ~/.macbootstrap/zsh-config/zsh_install.sh
+
 # start install
+echo ">>>>>>>>>> cli_install run <<<<<<<<<<"
+sh ~/.macbootstrap/install-steps/cli_install.sh
+echo ">>>>>>>>>> software_install run<<<<<<<<<<"
 sh ~/.macbootstrap/install-steps/software_install.sh
-sh ~/.macbootstrap/install-steps/terminal_install.sh
+
 # git config
+echo ">>>>>>>>>> git_install run <<<<<<<<<<"
 sh ~/.macbootstrap/git-config/git_install.sh
-# brew install python3
-# sh ~/.macbootstrap/install-steps/python_install.sh
-# node config
-sh ~/.macbootstrap/node-config/node_install.sh
+
+# python
+echo ">>>>>>>>>> python_install run <<<<<<<<<<"
+sh ~/.macbootstrap/install-steps/python_install.sh
+
+# nvm config
+echo ">>>>>>>>>> nvm_install run <<<<<<<<<<"
+sh ~/.macbootstrap/node-config/nvm_install.sh
 
 # 设置 mac (关闭 guest | 隐藏 Docker)
+echo ">>>>>>>>>> mac setting run <<<<<<<<<<"
 sudo sh ~/.macbootstrap/mac-config/macos.sh
 
 # vim config
+echo ">>>>>>>>>> vim setting run <<<<<<<<<<"
 sudo sh ~/.macbootstrap/vim-config/vim_install.sh
 
 # 取消代理
 unset ALL_PROXY
 
+# 开机脚本
+echo ">>>>>>>>>> hook_login run <<<<<<<<<<"
 ~/.macbootstrap/mac-config/hook_login.sh
