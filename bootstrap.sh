2d9f2dfe7e2eb80873f08f12a8b3d6636c8fdfd8
skybrim <throughskybrim@gmail.com>
2020-06-22 17:18:40 +0800

    hosts 优化

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ fi
 
 # hosts 中添加 raw.githubusercontent.com，防止 DNS 污染
 sudo -- sh -c "echo 199.232.68.133 raw.githubusercontent.com >> /etc/hosts"
+sudo -- sh -c "echo 199.232.68.133 camo.githubusercontent.com >> /etc/hosts"
 
 # install brew
 if [[ ! -e /usr/local/bin/brew ]]; then
