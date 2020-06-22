4623fc7affa7d9374937c729376d586efb6b5c8f
skybrim <throughskybrim@gmail.com>
2020-06-22 11:29:09 +0800

    hosts 优化

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ if [[ -e ~/.macbootstrap ]]; then
   rm -rf ~/.macbootstrap
 fi
 
+# hosts 中添加 raw.githubusercontent.com，防止 DNS 污染
+sudo -- sh -c "echo 199.232.68.133 raw.githubusercontent.com >> /etc/hosts"
+
 # install brew
 if [[ ! -e /usr/local/bin/brew ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
