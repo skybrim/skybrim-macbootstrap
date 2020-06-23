bd2ae0778cc3714072ac8ea6b4d6463614355c77
skybrim <throughskybrim@gmail.com>
2020-06-23 11:28:16 +0800

    hosts + nvm

--- nvm/nvm_install.sh
+++ nvm/nvm_install.sh
@@ -4,7 +4,9 @@ source $HOME/.macbootstrap/basic.sh
 # nvm
 echo ">>>>>>>>>> nvm install <<<<<<<<<<"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install node
+nvm install stable
+nvm alias default stable
+ln -s $(which node) /usr/local/bin/node
 npm config -g set registry https://registry.npm.taobao.org
 
 # npm install -g gnomon
