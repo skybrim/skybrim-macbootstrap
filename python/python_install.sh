7b3d36d493c4c25f897072e5febca7396cfe3e39
skybrim <throughskybrim@gmail.com>
2020-07-27 09:16:14 +0800

    fix

--- python/python_install.sh
+++ python/python_install.sh
@@ -13,14 +13,3 @@ if [[ ! -e /Applications/PyCharm.app ]]; then
 else
     echo "You have installed PyCharm"
 fi
-
-
-
-# pip3 install flake8
-# pip3 install yapf
-# pip3 install autoflake
-# pip3 install isort
-# pip3 install coverage
-# pip3 install pynvim
-# pip3 install Markdown2PDF
-# pip3 install pylint-django
--- zsh-config/zsh_install.sh
+++ zsh-config/zsh_install.sh
@@ -12,10 +12,12 @@ ln -s ~/.macbootstrap/zsh-config/.zshrc ~/.zshrc
 
 echo ">>>>>>>>>> oh-my-zsh theme spaceship-prompt <<<<<<<<<<"
 git clone https://github.com/denysdovhan/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship-prompt
-ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
 
 echo ">>>>>>>>>> oh-my-zsh plugins zsh-syntax-highlighting <<<<<<<<<<"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 echo ">>>>>>>>>> oh-my-zsh plugins zsh-autosuggestions <<<<<<<<<<"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+rm -rf ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
