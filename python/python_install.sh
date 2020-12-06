9a18e1566aee7266bc90e8a389717ff3257e472c
wiley <throughskybrim@gmail.com>
2020-12-06 22:19:45 +0800

    python

--- python/python_install.sh
+++ python/python_install.sh
@@ -4,15 +4,15 @@ source $HOME/.macbootstrap/basic.sh
 echo ">>>>>>>>>> brew cask install miniconda <<<<<<<<<<"
 brew cask install miniconda
 conda init zsh
-sudo chmod 777 ~/.conda/*
+# sudo chmod 777 ~/.conda/*
 
-# pycharm
-if [[ ! -e /Applications/PyCharm.app ]]; then
-    echo ">>>>>>>>>> brew cask install pycharm <<<<<<<<<<"
-    brew cask install pycharm
-else
-    echo "You have installed PyCharm"
-fi
+# # pycharm
+# if [[ ! -e /Applications/PyCharm.app ]]; then
+#     echo ">>>>>>>>>> brew cask install pycharm <<<<<<<<<<"
+#     brew cask install pycharm
+# else
+#     echo "You have installed PyCharm"
+# fi
 
 # spider
 # pip install requests
