349f295bcefa71cd9b9aa9ef44c5e73a86e62e64
Wiley <throughskybrim@gmail.com>
2020-12-07 10:19:42 +0800

    rvm

--- install-steps/cli_install.sh
+++ install-steps/cli_install.sh
@@ -72,9 +72,9 @@ echo ">>>>>>>>>> rvm install <<<<<<<<<<"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 echo "ruby_url=https://cache.ruby-china.com/pub/ruby" > ~/.rvm/user/db
-rvm install 2.6.6 --disbale-binary
-rvm use 2.6.6 --default
-gem install cocoapods
+# rvm install 2.6.6 --disbale-binary
+# rvm use 2.6.6 --default
+# gem install cocoapods
 
 # 汇编编译器
 echo ">>>>>>>>>> brew install yasm <<<<<<<<<<"
