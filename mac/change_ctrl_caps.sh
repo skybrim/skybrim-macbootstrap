95a2ee16970b5014df78b7f4569c396eb89d3382
wiley <throughskybrim@gmail.com>
2021-03-06 23:44:40 +0800

    add function opt_cmd

--- mac/change_ctrl_caps.sh
+++ mac/change_ctrl_caps.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# https://developer.apple.com/library/archive/technotes/tn2450/_index.html
+
 function hhkb() {
     hidutil property --set '{
         "UserKeyMapping":[
@@ -15,7 +17,22 @@ function hhkb() {
     }'
 }
 
-function dishhkb(){
+function opt_cmd() {
+    hidutil property --set '{
+        "UserKeyMapping":[
+            {
+            "HIDKeyboardModifierMappingSrc":0x7000000E2,
+            "HIDKeyboardModifierMappingDst":0x7000000E3,
+            },
+            {
+            "HIDKeyboardModifierMappingSrc":0x7000000E3,
+            "HIDKeyboardModifierMappingDst":0x7000000E2,
+            }
+        ]
+    }'
+}
+
+function keyMapClear(){
 	hidutil property --set '{
 	  "UserKeyMapping": []
 	}'
