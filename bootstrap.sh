647c3e71a43ba71791e86c27ec8c161073d97ff4
Wiley <throughskybrim@gmail.com>
2023-04-28 14:19:35 +0800

    使用ustc源安装homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!bin/sh
 
-if grep "github" /etc/hosts > /dev/null; then
-  echo ">>>>>>>>>> You have fixed hosts <<<<<<<<<<"
-else
-  # hosts 中添加 raw.githubusercontent.com，防止 DNS 污染
-  sudo -- sh -c "echo 199.232.68.133 raw.githubusercontent.com >> /etc/hosts"
-  sudo -- sh -c "echo 199.232.68.133 camo.githubusercontent.com >> /etc/hosts"
-  sudo -- sh -c "echo 199.232.68.133 cloud.githubusercontent.com >> /etc/hosts"
-fi
+# if grep "github" /etc/hosts > /dev/null; then
+#   echo ">>>>>>>>>> You have fixed hosts <<<<<<<<<<"
+# else
+#   # hosts 中添加 raw.githubusercontent.com，防止 DNS 污染
+#   sudo -- sh -c "echo 199.232.68.133 raw.githubusercontent.com >> /etc/hosts"
+#   sudo -- sh -c "echo 199.232.68.133 camo.githubusercontent.com >> /etc/hosts"
+#   sudo -- sh -c "echo 199.232.68.133 cloud.githubusercontent.com >> /etc/hosts"
+# fi
 
 # install brew
 if [[ ! -e /usr/local/bin/brew ]]; then
@@ -15,10 +15,8 @@ if [[ ! -e /usr/local/bin/brew ]]; then
   export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
   export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
   export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
-  /bin/bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/Homebrew/install@HEAD/install.sh)"
-	unset HOMEBREW_BREW_GIT_REMOTE
-	unset HOMEBREW_CORE_GIT_REMOTE
-	unset HOMEBREW_BOTTLE_DOMAIN
+  export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+  /bin/bash -c "$(curl -fsSL https://mirrors.ustc.edu.cn/misc/brew-install.sh)"
 else
   echo ">>>>>>>>>> You have installed brew <<<<<<<<<<"
 fi
